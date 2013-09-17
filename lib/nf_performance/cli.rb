ab12cc1074fe44e91f50a47bf5b31d0e960f7161
Andrew <andrew@mootpointer.com>
2013-09-17 16:58:43 +1000

    Add preliminary local interface

--- lib/nf_performance/cli.rb
+++ lib/nf_performance/cli.rb
@@ -11,8 +11,19 @@ module NFPerformance
     method_option :region, type: :string, required: true
     method_option :name, type: :string
 
-    def flood url
-      test_plan.run
+    def flood domain
+      test_plan.flood domain
+    end
+
+    desc "jmeter DOMAIN", "run jmeter using our test plan and options"
+    method_option :ramp, type: :numeric, required: true
+    method_option :users, type: :numeric, required: true
+    method_option :length, type: :numeric, required: true
+    method_option :region, type: :string, required: true
+    method_option :name, type: :string
+
+    def jmeter domain
+      test_plan.jmeter domain
     end
 
     no_commands do
